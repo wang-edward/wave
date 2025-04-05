700b8b15d494d747a717af7b5a5f446b84c09648
Edward Wang <edawordwang@gmail.com>
2025-04-04 20:32:05 -0400

    better colors

--- resampler/analysis.py
+++ resampler/analysis.py
@@ -14,27 +14,29 @@ def compute_spectrogram(audio_path):
 D1, sr1 = compute_spectrogram('samples/Trumpet.wav')
 D2, sr2 = compute_spectrogram('report/Trumpet_wavetable.wav')
 
-# Create figure with three subplots
-plt.figure(figsize=(15, 12))
+# Create figure with 2x2 grid
+plt.figure(figsize=(15, 10))
 
-# Plot original spectrogram (red)
-plt.subplot(3, 1, 1)
-librosa.display.specshow(D1, sr=sr1, x_axis='time', y_axis='log', cmap='Reds')
+# Plot original spectrogram (magma)
+plt.subplot(2, 2, 1)
+librosa.display.specshow(D1, sr=sr1, x_axis='time', y_axis='log', cmap='magma')
 plt.colorbar(format='%+2.0f dB')
 plt.title('Original Trumpet Sample (Bflat3)')
 
-# Plot wavetable spectrogram (blue)
-plt.subplot(3, 1, 2)
-librosa.display.specshow(D2, sr=sr2, x_axis='time', y_axis='log', cmap='Blues')
+# Plot wavetable spectrogram (viridis)
+plt.subplot(2, 2, 2)
+librosa.display.specshow(D2, sr=sr2, x_axis='time', y_axis='log', cmap='viridis')
 plt.colorbar(format='%+2.0f dB')
 plt.title('Wavetable Recreation of Trumpet (Bflat3)')
 
-# Plot combined spectrogram
-plt.subplot(3, 1, 3)
-librosa.display.specshow(D1, sr=sr1, x_axis='time', y_axis='log', cmap='Reds', alpha=0.5)
-librosa.display.specshow(D2, sr=sr2, x_axis='time', y_axis='log', cmap='Blues', alpha=0.5)
+# Plot combined spectrogram (spanning bottom row)
+plt.subplot(2, 1, 2)
+# Plot original first with higher alpha
+librosa.display.specshow(D1, sr=sr1, x_axis='time', y_axis='log', cmap='magma', alpha=0.7)
+# Plot wavetable second with lower alpha
+librosa.display.specshow(D2, sr=sr2, x_axis='time', y_axis='log', cmap='viridis', alpha=0.3)
 plt.colorbar(format='%+2.0f dB')
-plt.title('Combined View (Original in Red, Wavetable in Blue)')
+plt.title('Combined View (Original in Magma, Wavetable in Viridis)')
 
 plt.tight_layout()
 
